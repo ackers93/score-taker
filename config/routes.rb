8d51639085cad1960e18172052e625c7f0c2de92
ackers93 <andrew.ackers@gmail.com>
2021-07-06 09:17:12 -0700

    added games as root index and half-styled

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :games
   devise_for :users
-  root to: "home#index"
+  root to: "games#index"
   # get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
